351569868b1b19ebdf25edd58471803998b492fa
Ibrahim El Khansa <itzbahb@gmail.com>
2025-07-01 05:04:41 +0300

    styled new pages

---
+++ src/app/contribute/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Contribute() {
+  return (
+    <div className="contribute-page">
+      <h2>Contribute To Where&apos;s My Shattafe</h2>
+    </div>
+  );
+}
